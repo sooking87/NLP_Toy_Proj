10e12f921ca33a9523e30813075e7dd60e2f9fa9
SooKyoung <sksohn01@naver.com>
2022-08-01 00:29:37 +0900

    2022-07-31-pc

--- lyrics_sentiment_dataset/get_dataset.py
+++ lyrics_sentiment_dataset/get_dataset.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv('./lyrics_sentiment_dataset/lyrics_sentiment_dataset_3.csv')
-print(df)
+print(df.shape)
+print(df.keys())
+print(df['neg_sentiment'].value_counts().tolist(), '\n')
+print(df['neu_sentiment'].value_counts().tolist(), '\n')
+print(df['pos_sentiment'].value_counts().tolist(), '\n')
+print(df['com_sentiment'].value_counts().tolist(), '\n') 
